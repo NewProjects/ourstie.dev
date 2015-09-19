cfb6da445e9492f0d7c588f541f14bb251cb2f10
Benny Cardenas <benitocardenas@gmail.com>
2015-09-19 15:36:46 -0500

    Content updated to show pricing and contact info

--- app/views/partials/footer.blade.php
+++ app/views/partials/footer.blade.php
@@ -11,22 +11,24 @@
 						<div class="footer-content">
 							<div class="row">
 								<div class="col-sm-4">
-									<p>Lorem ipsum dolor sit amet, consect tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim ven.</p>
+									<p>Please contact us with any questions you may have.  We'd like to discuss your goals for your online presence.
+									</p>
 								</div>
 								<div class="col-sm-4">
 									<ul class="social-links circle">
 										<li class="facebook"><a target="_blank" href="http://www.facebook.com"><i class="fa fa-facebook"></i></a></li>
 										<li class="twitter"><a target="_blank" href="http://www.twitter.com"><i class="fa fa-twitter"></i></a></li>
-										<li class="skype"><a target="_blank" href="http://www.skype.com"><i class="fa fa-skype"></i></a></li>
-										<li class="linkedin"><a target="_blank" href="http://www.linkedin.com"><i class="fa fa-linkedin"></i></a></li>
+										{{-- <li class="skype"><a target="_blank" href="http://www.skype.com"><i class="fa fa-skype"></i></a></li>
+										<li class="linkedin"><a target="_blank" href="http://www.linkedin.com"><i class="fa fa-linkedin"></i></a></li> --}}
 									</ul>
 								</div>
 								<div class="col-sm-4">
 									<ul class="list-icons">
-										<li><i class="fa fa-map-marker pr-10"></i> One infinity loop, 54100</li>
-										<li><i class="fa fa-phone pr-10"></i> +00 1234567890</li>
-										<li><i class="fa fa-fax pr-10"></i> +00 1234567891 </li>
-										<li><i class="fa fa-envelope-o pr-10"></i> info@idea.com</li>
+										{{-- <li><i class="fa fa-map-marker pr-10"></i> One infinity loop, 54100</li> --}}
+										<li><i class="fa fa-phone pr-10"></i>(210)322-0876</li>
+										{{-- <li><i class="fa fa-phone pr-10"></i>(210)</li> --}}
+										{{-- <li><i class="fa fa-fax pr-10"></i> +00 1234567891 </li> --}}
+										<li><i class="fa fa-envelope-o pr-10"></i>info@pivotwebdev.digital</li>
 									</ul>
 								</div>
 							</div>
@@ -43,7 +45,9 @@
 			<div class="container">
 				<div class="row">
 					<div class="col-md-6">
-						<p>Copyright © 2014 iDea by <a target="_blank" href="http://htmlcoder.me">HtmlCoder</a>. All Rights Reserved</p>
+						<p>Copyright © 2015 by Pivot Web Development
+							{{-- <a target="_blank" href="http://htmlcoder.me">HtmlCoder</a>.  --}}
+							- All Rights Reserved</p>
 					</div>
 					<div class="col-md-6">
 						<nav class="navbar navbar-default" role="navigation">
